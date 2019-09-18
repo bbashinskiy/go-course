ff60465fd8eae9d835932728b44650c069338612
Bohdan Bashynskyi <generydey@localhost.localdomain>
2019-09-18 13:01:25 +0200

    Playing with declaration

---
+++ 03-hello-world/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 43
+	fmt.Println(x)
+	x = 54
+	y := x + 32
+	fmt.Println(y)
+	textnumber := "I will be old in"
+	fmt.Println(textnumber, y, "years.")
+}
