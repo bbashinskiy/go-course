77d53a1b06ef6f0facdb0f656293563539e4a9a6
Bohdan Bashynskyi <generydey@localhost.localdomain>
2019-10-29 14:52:08 +0100

    Playing with loops

---
+++ 033-for-statement/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 1
+	for x < 10 {
+		fmt.Println(x)
+		x *= 2
+	}
+	fmt.Println("done.")
+
+	x = 1
+	for {
+		if x > 10 {
+			break
+		}
+		fmt.Println(x)
+		x *= 2
+	}
+	fmt.Println("done.")
+}
